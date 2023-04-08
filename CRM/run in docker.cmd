0aab2b5547ee9884f8397798761e91794c50f90f
Egiazaryan Naira <egiazarian.naira@gmail.com>
2023-04-08 17:42:34 +0400

    Created initial web application.

---
+++ CRM/run in docker.cmd
@@ -0,0 +1,1 @@
+docker-compose -p crm build && docker-compose up
---
+++ CRM/src/CrmWeb/Controllers/TasksController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrmWeb.Controller
+{
+    [Route("tasks")]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet("test")]
+        public IActionResult Index()
+        {
+            return Ok("Working");
+        }
+    }
+}
